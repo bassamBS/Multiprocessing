ea70ca4a9ac6d892c03363660270c0471b9fd923
bassamBS <bassam-dev@posteo.net>
2023-01-07 22:33:10 +0100

    ✨ Add fifo writer logic

--- Named pipes/fifoWriter.c
+++ Named pipes/fifoWriter.c
@@ -7,3 +7,21 @@
 #include <sys/types.h>
 
 #define MaxWrite 256
+
+int main() {
+  srand(time(0)); // Générateur de nombre aléatoire
+
+  const char* fifoName = "./myPipel";
+  mkfifo(fifoName, 0666); // Lecture/Écriture pour user/group/others
+  int fd = open(fifoName, O_CREAT |O_WRONLY); // Ouvre un bloc sur un FIFO
+
+  int i;
+  for (i = 0; i < MaxWrite; i++) {
+    int n = rand();
+    write(fd, &n, sizeof(int));
+  }
+
+  close(fd); // Ferme le FIFO
+  unlink(fifoName); // Se débarasse du fichier sous-jacent
+  return 0;
+}
