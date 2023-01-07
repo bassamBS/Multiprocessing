b62bbff6d6fcc6ebe4a2a593ace8905690e67f0e
bassamBS <bassam-dev@posteo.net>
2023-01-07 19:54:05 +0100

    🎉  Import libraries

---
+++ multiprocessing.c
@@ -0,0 +1,8 @@
+/** Ce script est un exemple d'appel système pour créer du multiprocessing **/
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
