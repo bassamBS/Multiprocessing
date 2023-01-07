21350faafa82f3fc619d7a53627b0ea170595139
bassamBS <bassam-dev@posteo.net>
2023-01-07 20:27:46 +0100

    🚧 Work on multiprocess logic

--- multiprocessing.c
+++ multiprocessing.c
@@ -14,3 +14,21 @@ void report_and_die(const char* msg) {
   exit(-1);
 }
 
+int main() {
+  int pipeFDs[2]; // Array de descripteurs de fichier
+  char buf;
+  const char* msg = "Cette phrase est écrite par le process parent et affichée par le process enfant\n";
+
+  if (pipe(pipeFDs) < 0) report_and_die("Erreur");
+
+  pid_t cpid = fork(); // Child process 
+  if (cpid < 0) report_and_die("Le child process n'a pas pu être crée");
+
+  
+  if (0 == cpid) { // Ce code est exécuté par le child process
+
+  } else { // Ce code est exécuté par le parent process
+
+  }
+
+}
