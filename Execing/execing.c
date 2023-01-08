cea1fced81490b5790f954b0e3c30e6ef1fc58ce
bassamBS <bassam-dev@posteo.net>
2023-01-08 14:56:52 +0100

    ✨ Add logic to script

--- Execing/execing.c
+++ Execing/execing.c
@@ -2,3 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+  char* const args[] = {"./fileStatus", "tmp", 0};
+  int ret = execv("./fileStatus", args);
+  if (-1 == ret) {
+    perror("execv(...) error");
+    exit(-1);
+  }
+  else 
+    printf("I'm here\n");
+  return 0;
+}
