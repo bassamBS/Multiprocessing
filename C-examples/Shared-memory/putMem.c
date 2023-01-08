dd68427660b0cca3179d822c14ffb84b20ddd584
bassamBS <bassam-dev@posteo.net>
2023-01-08 19:49:20 +0100

    ✨ Import libraries

---
+++ C-examples/Shared-memory/putMem.c
@@ -0,0 +1,10 @@
+// Ce fichier est responsable de l'allocation de mémoire et de mettre une donnée
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
