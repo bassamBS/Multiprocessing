db96392f3f11e293ec234828d74c3f34993e4b8c
bassamBS <bassam-dev@posteo.net>
2023-01-08 19:56:41 +0100

    ✨ Add logic

--- C-examples/Shared-memory/putMem.c
+++ C-examples/Shared-memory/putMem.c
@@ -13,3 +13,21 @@ void report_and_die(const char* msg) {
   exit(-1);
 }
 
+char *setup(key_t key, int byte_count, int flags) {
+  // Obtient la mémoire partagée et son identifiant
+  int mem_id = shmget(key, byte_count, flags);
+
+  if (mem_id < 0) report_and_die("failed on shmget");
+
+  // Joint le segment de mémoire dans l'adresse mémoire de ce script
+  char* mem_ptr = shmat(mem_id, 0, 0); // 0 signifie qu'on laisse l'OS choisir
+  if (mem_ptr == (void*) - 1) report_and_die("failed_on_shmat");
+  
+  return mem_ptr;
+}
+
+int main() {
+  const char* greeting = "Hello world!";
+  int len = strlen(greeting) + 1;
+  key_t key = 9876;
+}
