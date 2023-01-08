074565fd3f1031a3fc8755fcb4a60f42c6a1c4b2
bassamBS <bassam-dev@posteo.net>
2023-01-08 15:53:21 +0100

    ✨ Add logic

--- node-example/cluster.js
+++ node-example/cluster.js
@@ -1,5 +1,23 @@
-import cluster from "cluster";
+import cluster from "cluster"; // Multiprocessing package
 import http from "http";
 import os from "os";
 
-console.log(os.cpus());
+const port = 8888;
+const processors = os.cpus();
+
+if (cluster.isPrimary) {
+  console.log(`Listening on port ${port}`);
+  console.log(`Processor count: ${processors.length}`);
+
+  // Fork un worker par processeur
+  processors.forEach((processor) => cluster.fork());
+  cluster.on("exit", (worker, code, signal) => cluster.fork()); // si un worker s'arrête, en créer un autre
+} else {
+  // Un serveur par processeur
+  http
+    .createServer((req, res) => {
+      res.writeHead(200);
+      res.end(`Echoeing request URI: " ${req.url} \n`);
+    })
+    .listen(port);
+}
